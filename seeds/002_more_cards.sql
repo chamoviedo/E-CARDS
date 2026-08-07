f461b8291d339d86be0cb78599e6c8ce15210c83
chamoviedo <chamoviedo5@gmail.com>
2026-08-07 07:39:16 -0300

    criei novo

---
+++ seeds/002_more_cards.sql
@@ -0,0 +1,23 @@
+INSERT INTO tbl_cards (hp, name, info, attack, damage, weak, resses, retreat, cardNumberInCollection, collection_id, type_id, stage_id)
+VALUES
+(120, 'Charizard', 'Flame Pokémon', 'Fire Spin', '100', 'Water', NULL, '3 Colorless', 4, 1, 1, 3),
+(60, 'Pikachu', 'Mouse Pokémon', 'Thunder Jolt', '30', 'Fighting', 'Steel', '1 Colorless', 58, 1, 4, 1),
+(90, 'Venusaur', 'Seed Pokémon', 'Solarbeam', '60', 'Fire', 'Water', '2 Colorless', 15, 1, 3, 3),
+(80, 'Blastoise', 'Shellfish Pokémon', 'Hydro Pump', '60+', 'Electric', NULL, '3 Colorless', 2, 1, 2, 3),
+(50, 'Bulbasaur', 'Seed Pokémon', 'Vine Whip', '20', 'Fire', 'Water', '1 Colorless', 44, 1, 3, 1),
+(50, 'Squirtle', 'Tiny Turtle Pokémon', 'Bubble', '10', 'Electric', NULL, '1 Colorless', 63, 1, 2, 1),
+(50, 'Charmander', 'Lizard Pokémon', 'Ember', '30', 'Water', NULL, '1 Colorless', 46, 1, 1, 1),
+(70, 'Raichu', 'Mouse Pokémon', 'Thunder', '60', 'Fighting', NULL, '2 Colorless', 14, 1, 4, 2),
+(40, 'Jigglypuff', 'Balloon Pokémon', 'Pound', '20', 'Fighting', 'Psychic', '1 Colorless', 54, 1, 7, 1),
+(60, 'Gengar', 'Shadow Pokémon', 'Nightmare', '30', 'Psychic', NULL, '2 Colorless', 27, 1, 5, 3),
+
+(70, 'Scyther', 'Mantis Pokémon', 'Slash', '30', 'Fire', 'Fighting', '1 Colorless', 10, 2, 3, 1),
+(80, 'Kangaskhan', 'Parent Pokémon', 'Comet Punch', '20x', 'Fighting', NULL, '3 Colorless', 5, 2, 7, 1),
+(60, 'Electrode', 'Ball Pokémon', 'Explosion', '80', 'Fighting', NULL, '1 Colorless', 2, 2, 4, 2),
+(90, 'Snorlax', 'Sleeping Pokémon', 'Body Slam', '30', 'Fighting', NULL, '4 Colorless', 11, 2, 7, 1),
+(70, 'Pidgeot', 'Bird Pokémon', 'Wing Attack', '30', 'Electric', NULL, '2 Colorless', 8, 2, 7, 2),
+(60, 'Nidoqueen', 'Drill Pokémon', 'Mega Punch', '40', 'Psychic', NULL, '3 Colorless', 7, 2, 6, 2),
+(50, 'Oddish', 'Weed Pokémon', 'Absorb', '20', 'Fire', 'Water', '1 Colorless', 6, 2, 3, 1),
+(60, 'Parasect', 'Mushroom Pokémon', 'Spore', '30', 'Fire', NULL, '2 Colorless', 9, 2, 3, 2),
+(70, 'Wigglytuff', 'Balloon Pokémon', 'Do the Wave', '10+', 'Fighting', 'Psychic', '2 Colorless', 19, 2, 7, 2),
+(80, 'Lapras', 'Transport Pokémon', 'Water Gun', '30+', 'Electric', NULL, '2 Colorless', 10, 3, 2, 1);
---
+++ seeds/003_more_cards.sql
@@ -0,0 +1,36 @@
+mare 30 records
+
+INSERT INTO tbl_cards (hp, name, info, attack, damage, weak, resses, retreat, cardNumberInCollection, collection_id, type_id, stage_id)
+VALUES
+(50, 'Abra', 'Psi Pokémon', 'Psyshock', '10', 'Psychic', NULL, '1 Colorless', 43, 1, 5, 1),
+(60, 'Kadabra', 'Psi Pokémon', 'Recover', '20', 'Psychic', NULL, '2 Colorless', 32, 1, 5, 2),
+(80, 'Alakazam', 'Psi Pokémon', 'Confuse Ray', '30', 'Psychic', NULL, '3 Colorless', 1, 1, 5, 3),
+(40, 'Machop', 'Superpower Pokémon', 'Low Kick', '20', 'Psychic', NULL, '1 Colorless', 52, 1, 6, 1),
+(60, 'Machoke', 'Superpower Pokémon', 'Karate Chop', '50', 'Psychic', NULL, '2 Colorless', 34, 1, 6, 2),
+(100, 'Machamp', 'Superpower Pokémon', 'Seismic Toss', '60', 'Psychic', NULL, '3 Colorless', 8, 1, 6, 3),
+(50, 'Gastly', 'Gas Pokémon', 'Lick', '10', 'Psychic', NULL, '1 Colorless', 33, 1, 5, 1),
+(60, 'Haunter', 'Gas Pokémon', 'Hypnosis', '20', 'Psychic', NULL, '2 Colorless', 29, 1, 5, 2),
+(80, 'Mewtwo', 'Genetic Pokémon', 'Psychic', '40+', 'Psychic', NULL, '3 Colorless', 10, 1, 5, 1),
+(60, 'Hitmonchan', 'Punching Pokémon', 'Special Punch', '40', 'Psychic', NULL, '2 Colorless', 7, 1, 6, 1),
+
+(70, 'Clefable', 'Fairy Pokémon', 'Metronome', '—', 'Fighting', 'Psychic', '2 Colorless', 1, 2, 7, 2),
+(60, 'Clefairy', 'Fairy Pokémon', 'Sing', '—', 'Fighting', 'Psychic', '1 Colorless', 5, 2, 7, 1),
+(80, 'Mr. Mime', 'Barrier Pokémon', 'Meditate', '10+', 'Psychic', NULL, '2 Colorless', 6, 2, 5, 1),
+(70, 'Tauros', 'Wild Bull Pokémon', 'Rampage', '20+', 'Fighting', NULL, '2 Colorless', 21, 2, 7, 1),
+(90, 'Pinsir', 'Stag Beetle Pokémon', 'Guillotine', '50', 'Fire', NULL, '2 Colorless', 9, 2, 3, 1),
+(60, 'Butterfree', 'Butterfly Pokémon', 'Whirlwind', '20', 'Fire', NULL, '1 Colorless', 3, 2, 3, 2),
+(50, 'Eevee', 'Evolution Pokémon', 'Tackle', '10', 'Fighting', NULL, '1 Colorless', 11, 2, 7, 1),
+(70, 'Flareon', 'Flame Pokémon', 'Flamethrower', '60', 'Water', NULL, '2 Colorless', 12, 2, 1, 2),
+(70, 'Vaporeon', 'Bubble Jet Pokémon', 'Water Gun', '30+', 'Electric', NULL, '2 Colorless', 13, 2, 2, 2),
+(70, 'Jolteon', 'Lightning Pokémon', 'Thunderbolt', '60', 'Fighting', NULL, '2 Colorless', 14, 2, 4, 2),
+
+(60, 'Aerodactyl', 'Fossil Pokémon', 'Wing Attack', '30', 'Electric', NULL, '2 Colorless', 1, 3, 7, 1),
+(80, 'Kabutops', 'Shellfish Pokémon', 'Slash', '30', 'Grass', NULL, '2 Colorless', 2, 3, 6, 2),
+(50, 'Kabuto', 'Shellfish Pokémon', 'Scratch', '10', 'Grass', NULL, '1 Colorless', 3, 3, 6, 1),
+(90, 'Omastar', 'Spiral Pokémon', 'Hydro Pump', '40+', 'Grass', NULL, '2 Colorless', 4, 3, 2, 2),
+(50, 'Omanyte', 'Spiral Pokémon', 'Water Gun', '10+', 'Grass', NULL, '1 Colorless', 5, 3, 2, 1),
+(70, 'Muk', 'Sludge Pokémon', 'Sludge', '30', 'Psychic', NULL, '2 Colorless', 6, 3, 5, 2),
+(60, 'Ditto', 'Transform Pokémon', 'Transform', '—', 'Fighting', NULL, '1 Colorless', 7, 3, 7, 1),
+(80, 'Articuno', 'Freeze Pokémon', 'Ice Beam', '30', 'Electric', NULL, '2 Colorless', 8, 3, 2, 1),
+(80, 'Zapdos', 'Electric Pokémon', 'Thunderstorm', '40', 'Fighting', NULL, '3 Colorless', 9, 3, 4, 1),
+(80, 'Moltres', 'Flame Pokémon', 'Dive Bomb', '80', 'Water', NULL, '3 Colorless', 10, 3, 1, 1);
