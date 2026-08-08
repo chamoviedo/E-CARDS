ab293dd19ba7fe89a8d64c7c5c7ac4b8e638f58d
chamoviedo <chamoviedo5@gmail.com>
2026-08-08 05:47:22 -0300

    criei

--- tables/001_create_tables.sql
+++ tables/001_create_tables.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE tbl_collections (
     id SERIAL PRIMARY KEY,
     collectionSetName VARCHAR(100) NOT NULL,
@@ -5,20 +6,25 @@ CREATE TABLE tbl_collections (
     totalCardsInCollection SMALLINT NOT NULL
 );
 
+
 CREATE TABLE tbl_types (
     id SERIAL PRIMARY KEY,
     typeName VARCHAR(30) NOT NULL UNIQUE
 );
+
 
 CREATE TABLE tbl_stages (
     id SERIAL PRIMARY KEY,
     stageName VARCHAR(20) NOT NULL UNIQUE
 );
 
+
 CREATE TABLE tbl_cards (
     id SERIAL PRIMARY KEY,
     hp SMALLINT,
     name VARCHAR(80) NOT NULL,
+    type_id INT,
+    stage_id INT,
     info TEXT,
     attack VARCHAR(100),
     damage VARCHAR(20),
@@ -29,7 +35,10 @@ CREATE TABLE tbl_cards (
     collection_id INT NOT NULL,
     type_id INT NOT NULL,
     stage_id INT NOT NULL,
-    CONSTRAINT fk_collection FOREIGN KEY (collection_id) REFERENCES tbl_collections (id) ON DELETE CASCADE,
-    CONSTRAINT fk_type FOREIGN KEY (type_id) REFERENCES tbl_types (id) ON DELETE RESTRICT,
-    CONSTRAINT fk_stage FOREIGN KEY (stage_id) REFERENCES tbl_stages (id) ON DELETE RESTRICT
+    CONSTRAINT fk_collection FOREIGN KEY (collection_id) 
+        REFERENCES tbl_collections (id) ON DELETE CASCADE,
+    CONSTRAINT fk_type FOREIGN KEY (type_id) 
+        REFERENCES tbl_types (id) ON DELETE RESTRICT,
+    CONSTRAINT fk_stage FOREIGN KEY (stage_id) 
+        REFERENCES tbl_stages (id) ON DELETE RESTRICT
 );
