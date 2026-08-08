032781369f7dfa40cad9bc7c81eddb315cf92c0f
chamoviedo <chamoviedo5@gmail.com>
2026-08-08 20:48:58 -0300

    atualizei

--- seeds/001_seeds_cards.sql
+++ seeds/001_seeds_cards.sql
@@ -1,16 +1,13 @@
-<--!consider tables bellow-->
-create a initial seed to all tables with tcg pokemon, generat inserts scripts
-
 -- Seed collections
 INSERT INTO tbl_collections (collectionSetName, release_date, totalCardsInCollection)
-VALUES
+VALUES 
 ('Base Set', '1999-01-09', 102),
 ('Jungle', '1999-06-16', 64),
 ('Fossil', '1999-10-10', 62);
 
 -- Seed types
 INSERT INTO tbl_types (typeName)
-VALUES
+VALUES 
 ('Fire'),
 ('Water'),
 ('Grass'),
@@ -21,21 +18,19 @@ VALUES
 
 -- Seed stages
 INSERT INTO tbl_stages (stageName)
-VALUES
+VALUES 
 ('Basic'),
 ('Stage 1'),
 ('Stage 2');
 
 -- Seed cards
-INSERT INTO tbl_cards (
-    hp, name, info, attack, damage, weak, resses, retreat,
-    cardNumberInCollection, collection_id, type_id, stage_id
-)
+INSERT INTO tbl_cards 
+(hp, name, info, attack, damage, weak, resses, retreat, cardNumberInCollection, collection_id, type_id, stage_id)
 VALUES
-(120, 'Charizard', 'Flame Pokémon', 'Fire Spin', '100', 'Water', NULL, '3 Colorless', 4, 1, 1, 3),
-(60, 'Pikachu', 'Mouse Pokémon', 'Thunder Jolt', '30', 'Fighting', 'Steel', '1 Colorless', 58, 1, 4, 1),
+(120, 'Charizard', 'Flame Pokémon', 'Fire Spin', '100', 'Water', 'None', '3 Colorless', 4, 1, 1, 3),
+(60, 'Pikachu', 'Mouse Pokémon', 'Thunder Jolt', '30', 'Fighting', 'None', '1 Colorless', 58, 1, 4, 1),
 (90, 'Venusaur', 'Seed Pokémon', 'Solarbeam', '60', 'Fire', 'Water', '2 Colorless', 15, 1, 3, 3),
-(70, 'Scyther', 'Mantis Pokémon', 'Slash', '30', 'Fire', 'Fighting', '1 Colorless', 10, 2, 3, 1),
-(80, 'Lapras', 'Transport Pokémon', 'Water Gun', '30+', 'Electric', NULL, '2 Colorless', 10, 3, 2, 1);
+(80, 'Scyther', 'Mantis Pokémon', 'Slash', '30', 'Fire', 'Fighting', '1 Colorless', 10, 2, 3, 1),
+(70, 'Lapras', 'Transport Pokémon', 'Water Gun', '30+', 'Electric', 'None', '2 Colorless', 10, 3, 2, 1);
 
-<--!generate 20 inserts records to table tbl_cards-->
+
